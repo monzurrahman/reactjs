140875f1c389adb2cdec248a3c4ba2e2cff624c2
monzur rahman <89568953+monzurrahman@users.noreply.github.com>
2025-03-27 10:55:22 +0600

    Create State.jsx
    In React, the useState Hook lets you add state to functional components. It gives you two things: the current state and a function to change it. You start by giving it an initial value, and whenever you use the function to update the state, React gives you the new value.

---
+++ useState/State.jsx
@@ -0,0 +1,33 @@
+// State.jsx starts here
+import { useState } from "react";
+export default function State(){
+
+    let[count, setCount] = useState(0)
+    let inState=()=> {
+        setCount(count += 1);
+       // console.log(count);
+    }
+    return (
+        <>
+        <h2>Count = {count}</h2>
+        <button onClick={inState}>Count increase</button>
+        </>
+    )
+}
+// App.jsx starts here
+//import { useState } from 'react'
+
+import './App.css'
+import State from "./State"
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>This is state</h1>
+      <State />
+    </>
+  )
+}
+
+export default App
